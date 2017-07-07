8f466c30869fb865ee02207bb83e691ce926ac13
Hichxbarre <hichammassamba@yahoo.fr>
2017-07-07 11:45:27 +0200

    ajout du dossier scripts

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+/*document.querySelector('html').onclick = function() {
+    alert('Aïe, arrêtez de cliquer !!');
+}
+*/
+//<button onload="document.getElementByClass('milieu').innerHTML=Date()">The time is?</button>
+
+/*function afficher_cacher(id)
+{
+    if(document.getElementById(id).style.visibility=="hidden")
+    {
+        document.getElementById(id).style.visibility="visible";
+        document.getElementById('bouton_'+id).innerHTML='Cacher le texte';
+    }
+    else
+    {
+        document.getElementById(id).style.visibility="hidden";
+        document.getElementById('bouton_'+id).innerHTML='Afficher le texte';
+    }
+    return true;
+}
+*/
+/*document.querySelector('html').onclick = function() {
+ afficher_cacher('milieu');
+}
+*/
+document.getElementById('milieu').style.visibility = "hidden";
